75e205dac72c3866daca082489ff7aa1f89f041b
Daniel Andrew <danwandrew@gmail.com>
2020-05-29 11:01:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,7 +29,7 @@ def count_sentences
 #puts self.split(/(\.|\?|\!)/)
 whitespace = self.split(' ')
 
-puts self.strip.split(/[?!.]/)
+puts self.strip.split(/[?!.]/).strip
 whitespace.count
 end
 end
