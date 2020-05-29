cc1010ef0b1d6e59b9e7e240123876169069d890
Daniel Andrew <danwandrew@gmail.com>
2020-05-29 10:31:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    puts self.split('.').split("!").split("?")
+    puts self.split('.').map {|words| words.split("?")}
     self.split('.').count
   end
 end
